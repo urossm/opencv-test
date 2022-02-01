23a3bbad329fc1505524997974e27507905cbc07
urossm <markovicc.uros@gmail.com>
2022-02-01 15:04:12 +0100

    Detekcija tacaka pomocu optical flow-a i dinamicki blend

--- android/src/main/jni/native_opencv.cpp
+++ android/src/main/jni/native_opencv.cpp
@@ -1,14 +1,18 @@
 #include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 #include <opencv2/stitching.hpp>
-#include <opencv2/imgproc.hpp>
-#include <android/log.h>
 #include <string>
+
+#include <android/log.h>
+
 using namespace cv;
 using namespace std;
 
 
 // Avoiding name mangling
 extern "C" {
+
     // Attributes to prevent 'unused' function from being removed and to make it visible
     __attribute__((visibility("default"))) __attribute__((used))
     const char* version() {
@@ -16,111 +20,29 @@ extern "C" {
         return CV_VERSION;
     }
 
+    __attribute__((visibility("default"))) __attribute__((used))
+    struct tokens: ctype<char>{
+        tokens(): std::ctype<char>(get_table()) {}
 
-__attribute__((visibility("default"))) __attribute__((used))
+        static std::ctype_base::mask const* get_table()
+        {
+            typedef std::ctype<char> cctype;
+            static const cctype::mask *const_rc= cctype::classic_table();
 
-struct tokens: ctype<char>
-{
-    tokens(): std::ctype<char>(get_table()) {}
+            static cctype::mask rc[cctype::table_size];
+            std::memcpy(rc, const_rc, cctype::table_size * sizeof(cctype::mask));
 
-    static std::ctype_base::mask const* get_table()
-    {
-        typedef std::ctype<char> cctype;
-        static const cctype::mask *const_rc= cctype::classic_table();
-
-        static cctype::mask rc[cctype::table_size];
-        std::memcpy(rc, const_rc, cctype::table_size * sizeof(cctype::mask));
-
-        rc[','] =  ctype_base::space;
-        rc[' '] =  ctype_base::space;
-        return &rc[0];
-    }
-};
-    vector<string> getpathlist(string path_string){
-         string sub_string = path_string.substr(1,path_string.length()-2);
-         stringstream ss(sub_string);
-        ss.imbue( locale( locale(), new tokens()));
-         istream_iterator<std::string> begin(ss);
-         istream_iterator<std::string> end;
-         vector<std::string> pathlist(begin, end);
-        return pathlist;
-    }
+            rc[','] =  ctype_base::space;
+            rc[' '] =  ctype_base::space;
+            return &rc[0];
+        }
+    };
 
     //------------------------MOJE FUNKCIJE--------------------------------
-
-    Mat blendImages(Mat blendedImages, Mat middleImg, int blendHeight, int blendWidth) {
-
-    	float alpha;
-
-    	int width = middleImg.cols;
-    	int height = middleImg.rows;
-    	int heightBlended = blendedImages.rows;
-
-    	int blend1Start = heightBlended/2 - height/2;
-    	int blend2Start = blend1Start + height - blendHeight;
-
-    	//Pravljenje prednje slike(cista kropovana panorama)
-    	Mat frontImg = Mat::zeros(heightBlended, width, middleImg.type());
-    	Mat temp(frontImg, Rect(0, blend1Start, width, height));
-    	middleImg.copyTo(temp);
-
-    	//Spajanje svega u jednu sliku koja sluzi kao pozadinska
-    	Mat backImg = blendedImages.clone();
-    	Mat temp4(backImg, Rect(0, blend1Start, width, height));
-    	middleImg.copyTo(temp4);
-
-    	//Pravljenje delica koji se kopira sa desne ivice na levu da bi se blendovao
-    	Mat backImgTemp = backImg.clone();
-    	backImgTemp = backImgTemp(Rect(width - blendWidth, 0, blendWidth, heightBlended));
-
-    	Mat backImgHoriz = backImg.clone();
-    	Mat temp2(backImgHoriz, Rect(0, 0, blendWidth, heightBlended));
-    	backImgTemp.copyTo(temp2);
-
-    	Mat finalImg = backImg.clone();
-
-    	//Prolazak kroz horizontalne ivice
-    	for (int i = 0; i < width; i++)
-    	{
-    		//Gornja ivica
-    		for (int j = blend1Start; j < blend1Start + blendHeight; j++)
-    		{
-    			alpha = 1 - (j - blend1Start) * (static_cast<float>(1) / blendHeight);
-
-    			finalImg.at<Vec3b>(j, i)[0] = (int)(blendedImages.at<Vec3b>(j, i)[0] * alpha) + backImg.at<Vec3b>(j, i)[0] * (1 - alpha);
-    			finalImg.at<Vec3b>(j, i)[1] = (int)(blendedImages.at<Vec3b>(j, i)[1] * alpha) + backImg.at<Vec3b>(j, i)[1] * (1 - alpha);
-    			finalImg.at<Vec3b>(j, i)[2] = (int)(blendedImages.at<Vec3b>(j, i)[2] * alpha) + backImg.at<Vec3b>(j, i)[2] * (1 - alpha);
-    		}
-
-    		//Donja ivica
-    		for (int j = blend2Start; j < blend2Start + blendHeight; j++)
-    		{
-    			alpha = (j - blend2Start) * (static_cast<float>(1) / blendHeight);
-
-    			finalImg.at<Vec3b>(j, i)[0] = (int)(blendedImages.at<Vec3b>(j, i)[0] * alpha) + backImg.at<Vec3b>(j, i)[0] * (1 - alpha);
-    			finalImg.at<Vec3b>(j, i)[1] = (int)(blendedImages.at<Vec3b>(j, i)[1] * alpha) + backImg.at<Vec3b>(j, i)[1] * (1 - alpha);
-    			finalImg.at<Vec3b>(j, i)[2] = (int)(blendedImages.at<Vec3b>(j, i)[2] * alpha) + backImg.at<Vec3b>(j, i)[2] * (1 - alpha);
-    		}
-    	}
-
-    	Mat final2Img = finalImg.clone();
-
-    	//Prolazak kroz jednu vertikalnu ivicu
-    	for (int i = 0; i < blendWidth; i++)
-    	{
-    		for (int j = 0; j < heightBlended; j++)
-    		{
-    			alpha = i * (static_cast<float>(1) / blendWidth);
-
-    			final2Img.at<Vec3b>(j, i)[0] = (int)(finalImg.at<Vec3b>(j, i)[0] * alpha + backImgHoriz.at<Vec3b>(j, i)[0] * (1 - alpha));
-    			final2Img.at<Vec3b>(j, i)[1] = (int)(finalImg.at<Vec3b>(j, i)[1] * alpha + backImgHoriz.at<Vec3b>(j, i)[1] * (1 - alpha));
-    			final2Img.at<Vec3b>(j, i)[2] = (int)(finalImg.at<Vec3b>(j, i)[2] * alpha + backImgHoriz.at<Vec3b>(j, i)[2] * (1 - alpha));
-    		}
-    	}
-
-    	return final2Img;
-    }
-
+    int calculateFinalWidth(Mat input);
+    int calculateFinalHeight(Mat inputBlended);
+    Mat blendSides(Mat input);
+    Mat addLogos(Mat input, Mat logo);
 
     void stitch_image(char* path) {
 
@@ -130,85 +52,176 @@ struct tokens: ctype<char>
         //Ucitavanje slike i dobijanje velicina
         string pathString = path;
 
-        __android_log_print(ANDROID_LOG_VERBOSE,"+++++++++++++++++OPENCV+++++++++++++++++", path);
+        __android_log_print(ANDROID_LOG_VERBOSE, "+++++++++++++++++OPENCV+++++++++++++++++", path);
 
         Mat img = imread(pathString + "/panoRaw.png");
         Mat logo = imread(pathString + "/logoRaw.png");
 
-        if(logo.empty()){
-            __android_log_print(ANDROID_LOG_VERBOSE,"+++++++++++++++++OPENCV+++++++++++++++++", "SLIKA LOGOA NIJE UCITANA!");
+        if (logo.empty()) {
+            __android_log_print(ANDROID_LOG_VERBOSE, "+++++++++++++++++OPENCV+++++++++++++++++", "SLIKA LOGOA NIJE UCITANA!");
         }
-        if(img.empty()){
-            __android_log_print(ANDROID_LOG_VERBOSE,"+++++++++++++++++OPENCV+++++++++++++++++", "SLIKA PANORAME NIJE UCITANA!");
+        if (img.empty()) {
+            __android_log_print(ANDROID_LOG_VERBOSE, "+++++++++++++++++OPENCV+++++++++++++++++", "SLIKA PANORAME NIJE UCITANA!");
         }
 
-        if(!img.empty() && !logo.empty()){
-            double heightOriginal = img.rows;
-            double widthOriginal = img.cols;
-            int blendHeight = heightOriginal*0.03;
-            int blendWidth = widthOriginal * 0.02;
+        if (!img.empty() && !logo.empty()) {
 
-            //Dobijanje pocetne i kranje koordinate za kropovanje(ovo se mora odraditi preko tensor flow-a da bi se dobile tacne koordinate)
-            int cropStart = widthOriginal * 0.05;
-            int cropEnd = (widthOriginal * 0.95) - cropStart + blendWidth;
-
-            //Izracunavanje velicina finalne slike na osnovu kropovanih koordinata
-            int heightAfter = heightOriginal * 1.65;
-            int widthAfter = cropEnd;
-
-            //Izracunavanje visina za watermark i za blurovane delove
-            int watermarkHeight = heightAfter * 0.18;
-            int cropBlurredHeight = heightAfter / 2 - heightOriginal / 2 - watermarkHeight;
-
-            //Kropovanje slike
-            Mat imgCrop = img(Rect(cropStart, 0, cropEnd, heightOriginal));
-            Mat imgCropTempTop = imgCrop.clone();
-            Mat imgCropTempBottom = imgCrop.clone();
-
-            //Gornji blurovani deo
-            Mat imgCropTop = imgCropTempTop(Rect( 0, 0, widthAfter, heightOriginal *0.05));
-            flip(imgCropTop, imgCropTop, 0);
-            resize(imgCropTop, imgCropTop, Size(widthAfter, cropBlurredHeight + blendHeight));
-            Mat imgCropTopBlurred;
-            blur(imgCropTop, imgCropTopBlurred, Size(150, 150));
-
-            //Donji blurovani deo
-            Mat imgCropBottom = imgCropTempBottom(Rect(0, heightOriginal * 0.8, widthAfter, heightOriginal * 0.2));
-            flip(imgCropBottom, imgCropBottom, 0);
-            resize(imgCropBottom, imgCropBottom, Size(widthAfter, cropBlurredHeight + blendHeight));
-            Mat imgCropBottomBlurred;
-            blur(imgCropBottom, imgCropBottomBlurred, Size(150, 150));
-
-            //Spajanje svih delova na glavnu veliku sliku
-            Mat imgFullSize = Mat(heightAfter, widthAfter, CV_8UC3, Scalar(168, 228, 19));
-
-            Mat topImage(imgFullSize, Rect(0, watermarkHeight, widthAfter, imgCropTopBlurred.rows));
-            imgCropTopBlurred.copyTo(topImage);
-
-            Mat bottomImage(imgFullSize, Rect(0, heightAfter/2 + heightOriginal/2 - blendHeight, widthAfter, imgCropBottomBlurred.rows));
-            imgCropBottomBlurred.copyTo(bottomImage);
-
-            Mat finalImage = blendImages(imgFullSize, imgCrop, blendHeight, blendWidth);
-            Mat finalImageCropped = finalImage(Rect(0, 0, finalImage.cols - blendWidth, finalImage.rows));
-
-            int logoWidth = finalImageCropped.cols;
-            int logoHeight = watermarkHeight;
-            Mat logoBottomLoaded = logo.clone();
-            Mat logoTopLoaded;
-            resize(logoBottomLoaded, logoBottomLoaded, Size(logoWidth, logoHeight), INTER_CUBIC);
-            rotate(logoBottomLoaded, logoTopLoaded, ROTATE_180);
-
-            Mat logoBottom(finalImageCropped, Rect(0, finalImageCropped.rows - logoHeight, logoWidth, logoHeight));
-            logoBottomLoaded.copyTo(logoBottom);
-
-            Mat logoTop(finalImageCropped, Rect(0, 0, logoWidth, logoHeight));
-            logoTopLoaded.copyTo(logoTop);
+            //Pozivanje funkcije za blendovanje leve i desne strane i funkcije za dodavanje logoa
+            __android_log_print(ANDROID_LOG_VERBOSE, "+++++++++++++++++OPENCV+++++++++++++++++", "POCETAK BLENDA");
+            Mat imgBlend = blendSides(img);
+            __android_log_print(ANDROID_LOG_VERBOSE, "+++++++++++++++++OPENCV+++++++++++++++++", "KRAJ BLENDA I POCETAK DODAVANJA LOGOA");
+            Mat imgFinal = addLogos(imgBlend, logo);
+            __android_log_print(ANDROID_LOG_VERBOSE, "+++++++++++++++++OPENCV+++++++++++++++++", "KRAJ DODAVANJA LOGOA");
 
             //Kraj i cuvanje slike
-            imwrite( pathString + "/panoFinal.jpg", finalImageCropped);
-            __android_log_print(ANDROID_LOG_VERBOSE,"+++++++++++++++++OPENCV+++++++++++++++++", "SLIKA OBRADJENA!");
+            imwrite(pathString + "/panoFinal.jpg", imgFinal);
         }
 
         //-----------------------------------------------------------------------------------------
+    }
+
+    Mat blendSides(Mat input) {
+
+        float alpha;
+
+        int width = input.cols;
+        int height = input.rows;
+
+        int cropWidth = calculateFinalWidth(input);
+
+        int blendWidth = cropWidth * 0.02;
+        int leftMargin = (width - cropWidth) / 2;
+
+        Mat finalImg(input, Rect(leftMargin, 0, cropWidth, height));
+        Mat blendPart(input, Rect(leftMargin + cropWidth, 0, blendWidth, height));
+
+        for (int i = 0; i < blendWidth; i++)
+        {
+            alpha = i * (static_cast<float>(1) / blendWidth);
+
+            for (int j = 0; j < height; j++)
+            {
+                finalImg.at<Vec3b>(j, i)[0] = (int)(finalImg.at<Vec3b>(j, i)[0] * alpha + blendPart.at<Vec3b>(j, i)[0] * (1 - alpha));
+                finalImg.at<Vec3b>(j, i)[1] = (int)(finalImg.at<Vec3b>(j, i)[1] * alpha + blendPart.at<Vec3b>(j, i)[1] * (1 - alpha));
+                finalImg.at<Vec3b>(j, i)[2] = (int)(finalImg.at<Vec3b>(j, i)[2] * alpha + blendPart.at<Vec3b>(j, i)[2] * (1 - alpha));
+            }
+        }
+
+        return finalImg;
+    }
+
+    Mat addLogos(Mat input, Mat logo) {
+
+        int height = input.rows;
+        int width = input.cols;
+
+        int finalHeight = calculateFinalHeight(input);
+        int finalWidth = width;
+
+        int logoHeight = (finalHeight - height) / 2;
+        int logoWidth = finalWidth;
+
+        Mat logoBottomInput = logo.clone();
+        Mat logoTopInput;
+        resize(logoBottomInput, logoBottomInput, Size(logoWidth, logoHeight), INTER_CUBIC);
+        rotate(logoBottomInput, logoTopInput, ROTATE_180);
+
+        Mat imgFinal = Mat(finalHeight, finalWidth, CV_8UC3, Scalar(168, 228, 19));
+
+        Mat mainImg(imgFinal, Rect(0, logoHeight, width, height));
+        input.copyTo(mainImg);
+
+        Mat logoBottom(imgFinal, Rect(0, finalHeight - logoHeight, logoWidth, logoHeight));
+        logoBottomInput.copyTo(logoBottom);
+
+        Mat logoTop(imgFinal, Rect(0, 0, logoWidth, logoHeight));
+        logoTopInput.copyTo(logoTop);
+
+        return imgFinal;
+
+    }
+
+    int calculateFinalHeight(Mat inputBlended) {
+
+        int height = inputBlended.rows;
+        int width = inputBlended.cols;
+
+        //Formula za finalnu visinu glasi: Uzmi odnos ulazne sirine i visine, podeli ga sa 2 i taj koeficient pomnozi sa ulaznom visinom
+        int heightAfter = static_cast<double>(width) / static_cast<double>(height) / 2.0 * static_cast<double>(height);
+
+        return heightAfter;
+    }
+
+    int calculateFinalWidth(Mat input) {
+
+        int width = input.cols;
+        int height = input.rows;
+
+        Mat leftImg(input, Rect(0, 0, width * 0.1, height));
+        Mat rightImg(input, Rect(width * 0.9, 0, width - width * 0.9, height));
+
+        Mat leftImgGray, rightImgGray;
+        Mat imgForDrawing = input.clone();
+
+        cvtColor(leftImg, leftImgGray, COLOR_BGR2GRAY);
+        cvtColor(rightImg, rightImgGray, COLOR_BGR2GRAY);
+
+        vector<Point2f> p0, p1;
+        vector<Point2f> p0_filtered, p1_filtered;
+
+        // Detekcija kornera na prvoj slici
+        goodFeaturesToTrack(leftImgGray, p0, 100, 0.3, 7, Mat(), 7, false, 0.04);
+
+        // Koriscenje optical flow-a za detekciju tacaka
+        vector<uchar> status;
+        vector<float> err;
+        TermCriteria criteria = TermCriteria((TermCriteria::COUNT)+(TermCriteria::EPS), 30, 0.01);
+        calcOpticalFlowPyrLK(leftImgGray, rightImgGray, p0, p1, status, err, Size(12, 12), 2, criteria);
+
+
+        for (uint i = 0; i < p0.size(); i++)
+        {
+            //Selektuj dobre tacke po statusu
+            if (status[i] == 1) {
+
+                //Dodaj ofset od 90% sirine posto se tacke pretrazuju samo na prvih 10% i poslednjih 10% ulazne slike
+                p1[i].x += width * 0.9;
+
+                int distanceX = p1[i].x - p0[i].x;
+                int distanceY = p1[i].y - p0[i].y;
+
+                if (abs(distanceY) < height * 0.02) {
+                    //Dodaj filterovane tacke u nove nizove
+                    p0_filtered.push_back(p0[i]);
+                    p1_filtered.push_back(p1[i]);
+
+                    //Ispisivanje i iscrtavanje tacaka i linija(samo za debug)
+                    cout << distanceX << endl;
+                    line(imgForDrawing, p1[i], p0[i], Scalar(0, 0, 255), 10);
+                    circle(imgForDrawing, p0[i], 1, Scalar(255, 0, 0), 10);
+                    circle(imgForDrawing, p1[i], 1, Scalar(255, 0, 0), 10);
+                }
+            }
+        }
+
+        double widthFinal = 0, p0_medium = 0, p1_medium = 0, p0_sum = 0, p1_sum = 0;
+
+        //Uzima sve filterovane tacke i nalazi sredinu iz njih
+        for (int i = 0; i < p0_filtered.size(); i++) {
+            p0_sum += p0_filtered[i].x;
+            p1_sum += p1_filtered[i].x;
+        }
+
+        p0_medium = p0_sum / p0_filtered.size();
+        p1_medium = p1_sum / p1_filtered.size();
+
+        widthFinal = p1_medium - p0_medium;
+
+        //Ako finalna sirina spada van ovih okvira, dodeli neku defaultnu vrednost
+        if (widthFinal < width * 0.88 || widthFinal > width * 0.93) {
+            widthFinal = width * 0.9;
+        }
+
+        return widthFinal;
     }
 }
